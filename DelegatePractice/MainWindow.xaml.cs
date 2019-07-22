d4f6166f983f51aff3329090b50da137cafc5ebe
Milo Fryling <milofryling@gmail.com>
2019-07-22 12:56:53 -0400

    // 3 parts: the object, the object manager, and the class(es) of functions that might be called by the manager to operate on the object         // instead of the manager being hard-coded, it can receive delegates.  That way new operation functions require no changes to the manager or the object.

--- DelegatePractice/MainWindow.xaml.cs
+++ DelegatePractice/MainWindow.xaml.cs
@@ -26,12 +26,23 @@ namespace DelegatePractice
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
+        // 3 parts: the object, the object manager, and the class(es) of functions that might be called by the manager to operate on the object
+        // instead of the manager being hard-coded, it can receive delegates.  That way new operation functions require no changes to the manager or the object.
         {
+            Console.WriteLine("Invoking the old way");
             new MainThingProcessor().DoProcesses();
+
+            Console.WriteLine("Invoking the new way");
+            //var processes = new MainThingProcessor.ProcessHandler();
+
+            new MainThingProcessor().DoProcesses(OriginalProcesses.DoProcessB);
+            new MainThingProcessor().DoProcesses(OriginalProcesses.DoProcessA);
+            new MainThingProcessor().DoProcesses(NewProcesses.DoProcessH);
+
         }
 
 
-        public static class ProcessesWeMightExtend
+        public static class OriginalProcesses
         {
             public static void DoProcessA(MainThing mainThing)
             {
@@ -43,17 +54,37 @@ namespace DelegatePractice
             }
 
         }
+
+        //added after first version.  Needs delegate so that this wouldn't require changes in the Processor
+        public static class NewProcesses
+        {
+            public static void DoProcessH(MainThing mainThing)
+            {
+                Console.WriteLine("PROCESS H!");
+            }
+        }
         public class MainThingProcessor
         {
             MainThing _mainThing = new MainThing();
 
-     
+            public delegate void ProcessHandler(MainThing mainThing);
+
+
+            //Old way, without delegate
             public void DoProcesses()
             {
-                ProcessesWeMightExtend.DoProcessA(_mainThing);
-                ProcessesWeMightExtend.DoProcessB(_mainThing);
-
+                OriginalProcesses.DoProcessA(_mainThing);
+                OriginalProcesses.DoProcessB(_mainThing);
             }
+
+            public void DoProcesses(ProcessHandler processHandler)
+            {
+                processHandler(_mainThing);
+            }
+
+
+
+
         }
 
         public class MainThing
